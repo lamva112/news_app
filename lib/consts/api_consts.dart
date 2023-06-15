affac57c986d2a3b697bfcc79cca3b309812cf25
lambui <lambui@lecle.co.kr>
2023-06-15 20:04:04 +0700

    first commit

---
+++ lib/consts/api_consts.dart
@@ -0,0 +1,5 @@
+String BASEURL = "newsapi.org";
+String API_KEY = "f8fb7f22f675400fab311ac2d9c38b6f";
+
+String BASEURL_FIREBASE =
+    "news-application-ec03f-default-rtdb.asia-southeast1.firebasedatabase.app";
